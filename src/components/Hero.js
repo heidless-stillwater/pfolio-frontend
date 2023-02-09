64534d4a41cf5625271cf6dd8816ebf1c0a1b5e5
heidless <rob.lockhart@yahoo.co.uk>
2023-02-09 17:58:33 +0000

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -120,7 +120,7 @@ const Hero = () => {
                                         >
                                             <Box 
                                                 component={LazyLoadImage}
-                                                src="/images/bg.jpg"
+                                                src='/images/bg.jpg'
                                                 alt='Background Image'
                                                 effect='blur'
                                                 height={{ xs: 'auto', md: 1 }}
